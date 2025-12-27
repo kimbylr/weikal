f1d26d95db39ea563e3c8e93732c05a195b319ce
Kim <21242860+kimbylr@users.noreply.github.com>
2025-12-27 16:39:37 +0100

    fix

--- scripts/users.ts
+++ scripts/users.ts
@@ -1,9 +1,12 @@
-import { users } from '../.auth';
-
-console.log(
-  users
-    .map(
-      ({ passphrase, calendarKey, heading }) => `${passphrase}:${calendarKey}:${heading}`,
-    )
-    .join(','),
-);
+import('../.auth')
+  .then((auth) => {
+    console.log(
+      auth.users
+        .map(
+          ({ passphrase, calendarKey, heading }) =>
+            `${passphrase}:${calendarKey}:${heading}`,
+        )
+        .join(','),
+    );
+  })
+  .catch(() => console.log('Need .auth.ts file at the project root.'));
